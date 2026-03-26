751b6afe3341f8c5fd961e2460a58a9ad06728e0
Caio Manoel Costa Leonardo <caio.m.c.leonardo@gmail.com>
2026-03-25 21:11:43 -0300

    Feature: Creation of three activities

---
+++ atividade3/script_bd_biblioteca_pessoal_v1.sql
@@ -0,0 +1,55 @@
+USE bibliotecapessoal;
+
+CREATE TABLE usuario (
+    id_usuario INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE autor (
+    id_autor INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    ano_nascimento YEAR,
+    ano_morte YEAR,
+    apresentacao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE categoria (
+    id_categoria INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(128) NOT NULL UNIQUE,
+    descricao TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE editora (
+    id_editora INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(255) NOT NULL UNIQUE,
+    cidade VARCHAR(64),
+    estado VARCHAR(64),
+    pais VARCHAR(64),
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+CREATE TABLE livro (
+    id_livro INT AUTO_INCREMENT PRIMARY KEY,
+    titulo VARCHAR(255) NOT NULL,
+    fk_usuario INT,
+    fk_autor INT,
+    fk_editora INT,
+    fk_categoria INT,
+    sinopse TEXT,
+    data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    data_atualizacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    lido BOOLEAN DEFAULT FALSE,
+    CONSTRAINT fk_livro_usuario FOREIGN KEY (fk_usuario) REFERENCES usuario(id_usuario) ON DELETE CASCADE,
+    CONSTRAINT fk_livro_autor FOREIGN KEY (fk_autor) REFERENCES autor(id_autor) ON DELETE RESTRICT,
+    CONSTRAINT fk_livro_editora FOREIGN KEY (fk_editora) REFERENCES editora(id_editora) ON DELETE RESTRICT,
+    CONSTRAINT fk_livro_categoria FOREIGN KEY (fk_categoria) REFERENCES categoria(id_categoria) ON DELETE RESTRICT
+);
